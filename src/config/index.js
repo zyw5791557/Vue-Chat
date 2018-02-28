4b7d1ff569f6705ef8672654647c70c376ae9fb5
Emlice <xiaomozyw@sina.com>
2018-02-28 14:35:22 +0800

    优化

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
 module.exports = {
-    $SOCKET_URL: 'http://localhost:3000',           // socket 服务地址
+    $SOCKET_URL: process.env.SOCKET_URL,            // socket 服务地址
     $STATIC_URL: 'http://localhost:8989',           // 资源服务器地址
 }
